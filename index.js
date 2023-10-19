91c6ebeceddb5442b7c277ee49220b1e4d0f818b
beko <126281068+beko-ojo@users.noreply.github.com>
2023-10-19 14:09:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+ 
+  function getWeather() {
+    const apiKey = '7ebca2935bde4770959182939231810';
+    const region = document.getElementById('city-input').value;
+    document.getElementById('city-name').innerHTML = region; 
+  
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${region}&aqi=yes`;
+  
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        document.getElementById('weather-icon').src = data.current.condition.icon;
+        document.getElementById('temperature').innerHTML = data.current.temp_c + '°C';
+        document.getElementById('weather-description').innerHTML = data.current.condition.text;
+  
+        console.log(data); 
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }
+  
